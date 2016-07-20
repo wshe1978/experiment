b3e53b16b448f2136f7f4b7083c1266285f9fe18
Wei She <wei@applatix.com>
2016-07-20 16:18:34 -0700

    Add a hello world test.

---
+++ tests/hello_world.py
@@ -0,0 +1,10 @@
+class HelloWorldTest(object):
+    """Hello world test."""
+
+    @staticmethod
+    def test():
+        print('Hello world!')
+
+
+if __name__ == '__main__':
+    HelloWorldTest().test()
