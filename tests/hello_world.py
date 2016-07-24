90b539002c0ffa1026516fab2c6ae4b7367f28e7
Wei She <wei@applatix.com>
2016-07-24 16:21:46 -0700

    Change hello world test.

--- tests/hello_world.py
+++ tests/hello_world.py
@@ -2,9 +2,9 @@ class HelloWorldTest(object):
     """Hello world test."""
 
     @staticmethod
-    def test():
+    def test_hello_world():
         print('Hello world!')
 
 
 if __name__ == '__main__':
-    HelloWorldTest().test()
+    HelloWorldTest().test_hello_world()
