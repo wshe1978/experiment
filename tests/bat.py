5bd093613ffbf1a5de260960313ef1b9ab894746
Wei She <wei@applatix.com>
2016-08-01 18:54:49 -0700

    Test2

---
+++ tests/bat.py
@@ -0,0 +1,10 @@
+class BasicAcceptanceTest(object):
+    """Basic acceptance test."""
+
+    @staticmethod
+    def test():
+        print('This is basic acceptance test!')
+
+
+if __name__ == '__main__':
+    BasicAcceptanceTest().test()
