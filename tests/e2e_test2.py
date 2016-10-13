dc315631e4106fbc1353011231b05ac6cd4c60b0
Wei She <wei@applatix.com>
2016-10-13 14:34:54 -0700

    Develop-2.

---
+++ tests/e2e_test2.py
@@ -0,0 +1,10 @@
+class E2ETest(object):
+    """End to end test."""
+
+    @staticmethod
+    def test():
+        print('Hello, this is an end-to-end TEST!')
+
+
+if __name__ == '__main__':
+    E2ETest().test()
